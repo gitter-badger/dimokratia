0e91f45701fcc1e7379f248131ab7f1b9dd719fc
semudu <serhat.md@gmail.com>
2014-12-13 17:28:01 +0200

    configuration

--- src/main/java/org/ygs/dimokratia/conf/Application.java
+++ src/main/java/org/ygs/dimokratia/conf/Application.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan
+@ComponentScan({"org.ygs.dimokratia.controller"})
 @EnableAutoConfiguration
 public class Application {
 
--- src/main/java/org/ygs/dimokratia/controller/HelloController.java
+++ src/main/java/org/ygs/dimokratia/controller/HelloController.java
@@ -1,15 +1,13 @@
 package org.ygs.dimokratia.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public @ResponseBody String hello(){
+    @RequestMapping("/hello")
+    public String hello(){
         return "hello";
     }
 }
